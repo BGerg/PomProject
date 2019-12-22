d03f5ae8faa36eb0a8ac68395b23d5c396975425
BGerg <example@sample.com>
2019-12-22 22:09:00 +0100

    three buttons added

--- pomodoro.py
+++ pomodoro.py
@@ -1,9 +1,50 @@
 #Pomodoro time app
 from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.core.window import Window
+
+
+class Buttons(RelativeLayout):
+    def __init__(self, **kwargs):
+        super(Buttons, self).__init__(**kwargs)
+
+
+        button_height = 100
+        button_width = 200
+
+
+        btn1 = Button(text ='START',
+                    font_size = 55,
+                    size = (button_width,button_height),
+                    pos = (20,20),
+                    background_color = (0, 245, 0, 1),
+                    size_hint =(None,None))
+        btn2 = Button(text='STOP',
+                    font_size = 55,
+                    pos = (300,20),
+                    background_color = (0, 0, 255, 1),
+                    size = (button_width,button_height),
+                    size_hint =(None,None))
+        btn3 = Button(text ='RESET',
+                    font_size =24,
+                    pos = (580,20),
+                    background_color = (245, 0, 0, 1),
+                    size = (button_width,button_height),
+                    size_hint = (None,None))
+
+        btn1.bind(on_press= lambda a:print("btn1 ok"))
+        btn2.bind(on_press= lambda a:print("btn2 ok"))
+        btn3.bind(on_press= lambda a:print("btn2 ok"))
+
+        self.add_widget(btn1)
+        self.add_widget(btn2)
+        self.add_widget(btn3)
 
 class PomodoroApp(App):
     def build(self):
-        return Button(text='Start', font_size=24, size =(200,100), size_hint=(None,None))
+        return Buttons()
 
-PomodoroApp().run()
+if __name__ == "__main__":
+    PomodoroApp().run()
