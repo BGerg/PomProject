3ac2b10cdb9433cbe1bfb5a549d00a71ba06af77
BGerg <example@sample.com>
2019-12-23 21:59:37 +0100

    circles added

--- pomodoro.py
+++ pomodoro.py
@@ -10,17 +10,23 @@ class Buttons(RelativeLayout):
     def __init__(self, **kwargs):
         super(Buttons, self).__init__(**kwargs)
 
-
+        percentage = 20
         button_height = 100
         button_width = 200
+
 
         with self.canvas:
             #Progress bar backgorund
             Color(0.26, 0.26, 0.26)
             Ellipse(pos=(225, 200), size=(350, 350))
+            #Circular progress line
+            Color(1, 0, 0)
+            Ellipse(pos=(225, 200), size=(350, 350),
+                    angle_end=(percentage))
             #Progress bar foreground
             Color(0, 0, 0)
             Ellipse(pos=(260, 235), size=(280, 280))
+
 
         btn1 = Button(text ='START',
                     font_size = 55,
@@ -41,7 +47,9 @@ class Buttons(RelativeLayout):
                     size = (button_width,button_height),
                     size_hint = (None,None))
 
-        btn1.bind(on_press= lambda a:print("btn1 ok"))
+        def callback(instance):
+            percentage += 30
+        btn1.bind(on_press= callback)
         btn2.bind(on_press= lambda a:print("btn2 ok"))
         btn3.bind(on_press= lambda a:print("btn2 ok"))
 
